c67602b7f40ffe760ea766250a5dc4bf0c5f5bf5
Satwik-Bhagwat <69520386+Satwik-Bhagwat@users.noreply.github.com>
2021-06-02 16:23:52 +0530

    Add files via upload

---
+++ Backend/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    spends: [{
+        _id: Number,
+        category: String,
+        amount: Number,
+        date: String,
+        time: String,
+        note: String
+    }]
+})
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
